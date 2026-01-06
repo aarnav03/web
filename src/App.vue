76c1b3d43c21e5f40e1b176bc62b46fe553bb7d2
arnv <rnv.gmx1438@gmail.com>
2026-01-07 01:34:24 +0530

    first

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+
+<template>
+  <main class="min-h-screen bg-neutral-900 text-white flex items-center justify-center">
+    <h1 class="text-4xl font-bold">It works</h1>
+  </main>
+</template>
---
+++ src/work.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <section>
+    <h2 class="text-xl font-semibold mb-8">
+      Work
+    </h2>
+
+    <div class="flex gap-8 overflow-x-auto pb-4">
+      <ProjectCard v-for="n in 4" :key="n" />
+    </div>
+  </section>
+</template>
+
+<script setup>
+import ProjectCard from "./proj.vue"
+</script>
