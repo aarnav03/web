480b7a00d72f916e687a467770d11f9972d73f7a
arnv <rnv.gmx1438@gmail.com>
2026-01-09 03:53:49 +0530

    layout and proj tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,110 @@
 <template>
-	<main class="page">
-		<section class="intro">
-			<h1>Aarnav</h1>
-			<h1>Panchal</h1>
-			<p>I try to create </p>
-		</section>
+  <main class="page">
+    <section class="intro">
+      <h1>Aarnav</h1>
+      <h1>Panchal</h1>
+      <p>I try to create</p>
+    </section>
 
-		
-		<section class="work">
-			<h2>Projects</h2>
-			<div class="projects">
-				<div class="card">
-  <div class="card-cover">
-    <img src="./assets/vimnavsmol.png" alt="vimnav cover" />
-  </div>
+    <section class="work">
+      <h2>Projects</h2>
+      <div class="projects">
+        <div class="card">
+          <div class="card-cover">
+            <img src="./assets/lc3.png" alt="lc3 cover" />
+          </div>
 
-  <div class="card-content">
-    <h3>vimnav</h3>
-    <p>vim but scuffed</p>
+          <div class="card-content">
+            <h3>lc3 vm</h3>
+            <p>vm based on lc3 architecture</p>
 
-    <div class="card-stack">
-      <img src="./assets/icons/c.svg" alt="c" />
-    </div>
-  </div>
-</div>
-<div class="card">
-  <div class="card-cover">
-    <img src="./assets/lc3.png" alt="lc3 cover" />
-  </div>
+            <div class="card-stack">
+              <img src="./assets/icons/c.svg" alt="c" />
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-cover">
+            <img src="./assets/vimnav3.png" alt="vimnav cover" />
+          </div>
 
-  <div class="card-content">
-    <h3>lc3 vm</h3>
-    <p>vm based on lc3 architecture </p>
+          <div class="card-content">
+            <h3>vimnav</h3>
+            <p>vim but scuffed</p>
 
-    <div class="card-stack">
-      <img src="./assets/icons/c.svg" alt="c" />
-    </div>
-  </div>
-</div>
+            <div class="card-stack">
+              <img src="./assets/icons/c.svg" alt="c" />
+            </div>
+          </div>
+        </div>
 
-<div class="card">
-  <div class="card-cover">
-    <img src="./assets/i3.jpg" alt="pixarch cover" />
-  </div>
+        <div class="card">
+          <div class="card-cover">
+            <img src="./assets/i3.jpg" alt="pixarch cover" />
+          </div>
 
-  <div class="card-content">
-    <h3>Pixarch</h3>
-    <p>Arch linux rice based on pixel theme</p>
+          <div class="card-content">
+            <h3>Pixarch</h3>
+            <p>Arch linux rice based on pixel theme</p>
 
-    <div class="card-stack">
-      <img src="./assets/icons/bash.svg" alt="bash" />
-    </div>
-  </div>
-</div>
+            <div class="card-stack">
+              <img src="./assets/icons/bash.svg" alt="bash" />
+            </div>
+          </div>
+        </div>
 
-				<!-- <div class="card"></div> -->
-				<!-- <div class="card"></div> -->
-			</div>
-<section class="skills">
-			<h2>Skills</h2>
+        <!-- <div class="card"></div> -->
+        <!-- <div class="card"></div> -->
+      </div>
+      <section class="skills">
+        <h2>Skills</h2>
 
-			<div class="skills-grid">
-				
+        <div class="skills-grid">
+          <div class="skill">
+            <img src="/src/assets/icons/c.svg" alt="c" />
+          </div>
+          <div class="skill">
+            <img src="/src/assets/icons/bash.svg" alt="bash" />
+          </div>
+          <div class="skill">
+            <img src="/src/assets/icons/git.svg" alt="git" />
+          </div>
 
-				
-				<div class="skill">
-					<img src="/src/assets/icons/c.svg" alt="c" />
-				</div>
-				<div class="skill">
-					<img src="/src/assets/icons/bash.svg" alt="bash" />
-				</div>
-<div class="skill">
-					<img src="/src/assets/icons/git.svg" alt="git" />
-				</div>
+          <div class="skill">
+            <img src="/src/assets/icons/docker.svg" alt="docker" />
+          </div>
 
-				<div class="skill">
-					<img src="/src/assets/icons/docker.svg" alt="docker" />
-				</div>
+          <div class="skill">
+            <img src="/src/assets/icons/py.svg" alt="python" />
+          </div>
 
-				<div class="skill">
-					<img src="/src/assets/icons/py.svg" alt="python" />
-				</div>
-				
-				<div class="skill">
-					<img src="/src/assets/icons/blender.svg" alt="blender" />
-				</div>
+          <div class="skill">
+            <img src="/src/assets/icons/blender.svg" alt="blender" />
+          </div>
 
-				<div class="skill">
-					<img src="/src/assets/icons/lua.svg" alt="lua" />
-				</div>
-			</div>
-		</section>
-			<section class="skills">
-				<div class="skills-grid">
-					<div class="skill" id="cn">
-						<a href="https://github.com/aarnav03"  target="_blank">
-						<img src="/src/assets/icons/ico/gh.svg" alt="github" />
-					</a>
-					</div>
+          <div class="skill">
+            <img src="/src/assets/icons/lua.svg" alt="lua" />
+          </div>
+        </div>
+      </section>
+      <section class="skills">
+        <div class="social">
+          <div class="skill" id="cn">
+            <a href="https://github.com/aarnav03" target="_blank">
+              <img src="/src/assets/icons/ico/gh.svg" alt="github" />
+            </a>
+          </div>
 
-					<div class="skill" id="cn">
-						<a href="https://www.linkedin.com/in/aarnav-panchal-0923952b7/" target="_blank" >
-						<img src="/src/assets/icons/ico/linkedin.svg" alt="linkedin" /></a>
-					</div>
-
-				</div>
-			</section>
-
-
-		</section>
-	</main>
+          <div class="skill" id="cn">
+            <a
+              href="https://www.linkedin.com/in/aarnav-panchal-0923952b7/"
+              target="_blank"
+            >
+              <img src="/src/assets/icons/ico/linkedin.svg" alt="linkedin"
+            /></a>
+          </div>
+        </div>
+      </section>
+    </section>
+  </main>
 </template>
