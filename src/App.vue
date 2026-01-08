dbb594cc409f2ebb01510ed5994409d4a112ddd6
arnv <rnv.gmx1438@gmail.com>
2026-01-09 00:39:12 +0530

    added projects,tweaks

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,14 @@
 			<h2>Skills</h2>
 
 			<div class="skills-grid">
-				<div class="skill">
-					<img src="/src/assets/icons/git.svg" alt="git" />
-				</div>
+				
 
 				<div class="skill">
 					<img src="/src/assets/icons/blender.svg" alt="blender" />
+				</div>
+<div class="skill">
+	
+					<img src="/src/assets/icons/bash.svg" alt="bash" />
 				</div>
 
 				<div class="skill">
@@ -33,9 +35,52 @@
 		<section class="work">
 			<h2>projects</h2>
 			<div class="projects">
-				<div class="card"></div>
-				<div class="card"></div>
-				<div class="card"></div>
+				<div class="card">
+  <div class="card-cover">
+    <img src="./assets/vimnavsmol.png" alt="vimnav cover" />
+  </div>
+
+  <div class="card-content">
+    <h3>vimnav</h3>
+    <p>vim but scuffed</p>
+
+    <div class="card-stack">
+      <img src="./assets/icons/c.svg" alt="c" />
+    </div>
+  </div>
+</div>
+<div class="card">
+  <div class="card-cover">
+    <img src="./assets/lc3.png" alt="lc3 cover" />
+  </div>
+
+  <div class="card-content">
+    <h3>lc3 vm</h3>
+    <p>vm based on lc3 architecture </p>
+
+    <div class="card-stack">
+      <img src="./assets/icons/c.svg" alt="c" />
+    </div>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-cover">
+    <img src="./assets/i3.jpg" alt="pixarch cover" />
+  </div>
+
+  <div class="card-content">
+    <h3>Pixarch</h3>
+    <p>Arch linux rice based on pixel theme</p>
+
+    <div class="card-stack">
+      <img src="./assets/icons/bash.svg" alt="bash" />
+    </div>
+  </div>
+</div>
+
+				<!-- <div class="card"></div> -->
+				<!-- <div class="card"></div> -->
 			</div>
 			<section class="skills">
 				<div class="skills-grid">
